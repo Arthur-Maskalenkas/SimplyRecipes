e221c4d3272a4a6cc4e142a0c8c5a3f0f503b19a
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-13 20:26:12 -0300

    404 add

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,12 @@ import Layout from '../components/Layout'
 
 const Error = () => (
   <Layout>
-    <p>Error page!</p>
+    <main className="error-page">
+      <section>
+        <h1>404</h1>
+        <h3>page not found</h3>
+      </section>
+    </main>
   </Layout>
 )
 
