04859740b65905fdea0bc03d805e10b15b0b1b5a
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-13 20:46:05 -0300

    contact page finish

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,46 @@ import Layout from '../components/Layout'
 
 const Contact = () => (
   <Layout>
-    <p>Contact page!</p>
+    <main className="page">
+      <section className="contact-page">
+        <article className="contact-info">
+          <h3>Want to get In Touch?</h3>
+          <p>
+            Four dollar toast biodiesel plaid salvia actually pickled banjo
+            bespoke mlkshk intelligentsia edison bulbp synth.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+            quo.
+          </p>
+          <p>
+            est repudiandae dolores vel earum. Illo voluptatem facere ullam
+            maiores amet aperiam recusandae quidem alias commodi, iusto ad
+            aspernatur mollitia?
+          </p>
+        </article>
+
+        <article>
+          <form className="form contact-form">
+            <div className="form-row">
+              <label htmlFor="name">your name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="email">your email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="message">message</label>
+              <textarea name="" id=""></textarea>
+            </div>
+            <button type="submit" className="btn btn-block">
+              submit
+            </button>
+          </form>
+        </article>
+      </section>
+    </main>
   </Layout>
 )
 
