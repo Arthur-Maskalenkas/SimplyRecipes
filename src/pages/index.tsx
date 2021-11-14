9cf44435dfcdb1b08e8f02a60d43d6a8879bc050
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-14 15:57:57 -0300

    banner in home page add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/Layout'
 
 const Index = () => (
   <Layout>
-    <p>Index page!</p>
+    <main className="page">
+      <header className="hero">
+        <StaticImage
+          src="../assets/images/main.jpeg"
+          alt="eggs"
+          className="hero-img"
+          placeholder="tracedSVG"
+          layout="fullWidth"
+        />
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1>simply recipes</h1>
+            <h4>no fluff, just recipes</h4>
+          </div>
+        </div>
+      </header>
+    </main>
   </Layout>
 )
 
