1f08fd0a58f6eccc35b2033b693bb03534d1e918
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-15 20:09:38 -0300

    queries add

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,36 +1,63 @@
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
-
-import * as React from 'react'
+import React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link, graphql } from 'gatsby'
+import RecipesList from '../components/RecipesList'
 
-const About = () => (
-  <Layout>
-    <main className="page">
-      <section className="about-page">
-        <article>
-          <h2>Im baby coloring book poke taxidermy</h2>
-          <p>
-            Taxidermy forage glossier letterpress heirloom before they sold out
-            you probably havent heard of them banh mi biodiesel chia.
-          </p>
-          <p>
-            Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
-            retro.
-          </p>
-          <Link to="/contact" className="btn">
-            contact
-          </Link>
-        </article>
-        <StaticImage
-          alt="Person pouring salt in bowl"
-          src="../assets/images/about.jpeg"
-          className="about-img"
-          placeholder="tracedSVG"
-        />
-      </section>
-    </main>
-  </Layout>
-)
+const About = ({ data }: any) => {
+  const recipes = data.allContentfulRecipe.nodes
+
+  return (
+    <Layout>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Im baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably havent heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
+      </main>
+    </Layout>
+  )
+}
 
 export default About
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,49 +1,77 @@
+import { graphql } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/Layout'
+import RecipesList from '../components/RecipesList'
 
-const Contact = () => (
-  <Layout>
-    <main className="page">
-      <section className="contact-page">
-        <article className="contact-info">
-          <h3>Want to get In Touch?</h3>
-          <p>
-            Four dollar toast biodiesel plaid salvia actually pickled banjo
-            bespoke mlkshk intelligentsia edison bulbp synth.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            quo.
-          </p>
-          <p>
-            est repudiandae dolores vel earum. Illo voluptatem facere ullam
-            maiores amet aperiam recusandae quidem alias commodi, iusto ad
-            aspernatur mollitia?
-          </p>
-        </article>
+const Contact = ({ data }: any) => {
+  const recipes = data.allContentfulRecipe.nodes
 
-        <article>
-          <form className="form contact-form">
-            <div className="form-row">
-              <label htmlFor="name">your name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="email">your email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="form-row">
-              <label htmlFor="message">message</label>
-              <textarea name="" id=""></textarea>
-            </div>
-            <button type="submit" className="btn btn-block">
-              submit
-            </button>
-          </form>
-        </article>
-      </section>
-    </main>
-  </Layout>
-)
+  return (
+    <Layout>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to get In Touch?</h3>
+            <p>
+              Four dollar toast biodiesel plaid salvia actually pickled banjo
+              bespoke mlkshk intelligentsia edison bulbp synth.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              quo.
+            </p>
+            <p>
+              est repudiandae dolores vel earum. Illo voluptatem facere ullam
+              maiores amet aperiam recusandae quidem alias commodi, iusto ad
+              aspernatur mollitia?
+            </p>
+          </article>
 
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="" id=""></textarea>
+              </div>
+              <button type="submit" className="btn btn-block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
+      </main>
+    </Layout>
+  )
+}
 export default Contact
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
