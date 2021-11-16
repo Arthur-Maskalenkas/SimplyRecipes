c11769f9c8278224f6d887bf7f5a3f7e163019bb
Arthur Maskalenkas <maskalenkasdoc@gmail.com>
2021-11-15 21:45:38 -0300

    fix

--- src/components/TagsList/index.tsx
+++ src/components/TagsList/index.tsx
@@ -1,4 +1,9 @@
+import { Link } from 'gatsby'
 import React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import setupTags from '../../utils/setupTags'
 
 type RecipesProps = {
   title: string
@@ -15,6 +20,20 @@ type TagsListProps = {
   recipes: RecipesProps[]
 }
 
-const TagsList = ({ recipes }: TagsListProps) => <div>this is tagsList</div>
+const TagsList = ({ recipes }: TagsListProps) => {
+  const newTags = setupTags(recipes)
 
+  return (
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag: any, index: any) => (
+          <Link to={`/${tag[0]}`} key={index}>
+            {tag[0]} ({tag[1]})
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
 export default TagsList
